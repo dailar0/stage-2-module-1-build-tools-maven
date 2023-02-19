5c48341e7cc941e453005d321f7966eabae9aaba
dailar <dailarse00@gmail.com>
2023-02-19 04:26:51 +0300

    done?

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return NumberUtils.toDouble(str) > 0;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
